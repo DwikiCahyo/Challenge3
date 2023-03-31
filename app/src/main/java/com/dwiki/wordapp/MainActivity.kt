da978a24e15294468f32b0fe3b4de839598b0a1b
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2023-03-31 18:30:51 +0700

    first commit

---
+++ app/src/main/java/com/dwiki/wordapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.dwiki.wordapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dwiki.wordapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = getString(R.string.str_title_appbar)
+
+        setupFragment()
+    }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,ListAlphabetFragment())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/dwiki/wordapp/adapter/AlphabetAdapter.kt
@@ -0,0 +1,57 @@
+package com.dwiki.wordapp.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.dwiki.wordapp.ContentFragment
+import com.dwiki.wordapp.ContentFragment.Companion.DATA
+import com.dwiki.wordapp.R
+import com.dwiki.wordapp.databinding.ListAlphabetItemBinding
+import com.dwiki.wordapp.model.Alphabet
+
+
+class AlphabetAdapter(private val listAlphabet:ArrayList<Alphabet>):RecyclerView.Adapter<AlphabetAdapter.ListViewHolder>() {
+
+    class ListViewHolder(var binding:ListAlphabetItemBinding):RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ListAlphabetItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+
+        val alphabet = listAlphabet[position]
+        holder.binding.apply {
+            tvAlphabetName.text = alphabet.alphabetName
+            ivAlphabet.setImageResource(alphabet.imgAlphabet)
+            cvAlphabetItem.setOnClickListener {
+
+                val appCompatActivity = it.context as AppCompatActivity
+                val contentFragment = ContentFragment()
+
+                val bundle = Bundle()
+                bundle.putParcelable(DATA,alphabet)
+                contentFragment(contentFragment, bundle, appCompatActivity)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = listAlphabet.size
+
+    private fun contentFragment(
+        contentFragment: ContentFragment,
+        bundle: Bundle,
+        appCompatActivity: AppCompatActivity
+    ) {
+        contentFragment.arguments = bundle
+        appCompatActivity.supportFragmentManager.beginTransaction()
+            .replace(R.id.container, contentFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+
+}
